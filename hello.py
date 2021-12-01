0a92e64d93c5d5356b38bf80672a110b99209a99
unknown <mohammedshahalatholi@gmail.com>
2021-12-01 02:49:09 -0500

    first commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+# d=len(a)
+b = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+# e=len(b)
+c=  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+# f=len(c)
+# print(d)
+for x in range(0 ,26):
+     for y in range(0,26):
+         for z in range(0,26):
+              print(a[x],b[y],c[z])
+       
+
+
+
+ 
+
+
+
+
+  
+   
